04fd205b331fac706ec3e5e0e415cd78584a7399
nortoxs <vivekkumar89751@gmail.com>
2024-12-15 19:57:51 +0530

    Update negative.py
    Signed-off-by: nortoxs <vivekkumar89751@gmail.com>

--- AnonXMusic/plugins/tools/negative.py
+++ AnonXMusic/plugins/tools/negative.py
@@ -15,7 +15,7 @@ async def send_poll(client, message):
         type=enums.PollType.QUIZ,  # Or use enums.PollType.REGULAR for a regular poll
         correct_option_id=0,  # Index of the correct option (0 for "Python" in this case)
         explanation="Python is known for being beginner-friendly and versatile.",
-        explanation_parse_mode="markdown",  # You can use markdown or html here
+        explanation_parse_mode=enums.ParseMode.MARKDOWN,  # You can use markdown or html here
         reply_markup=InlineKeyboardMarkup([
             [InlineKeyboardButton("Try another poll", callback_data="another_poll")]
         ])
